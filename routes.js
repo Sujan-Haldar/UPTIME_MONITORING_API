c4d14ed738b8b8dba961f7612b9aabfed129362e
Sujan Haldar <hsujan122@gmail.com>
2023-08-14 00:11:24 +0530

    Commit on 14/07/2023

--- routes.js
+++ routes.js
@@ -1,16 +1,13 @@
 // Dependency
-const {sampleHandler} = require("./handlers/routeHandlers/sampleHandler");
-const {userHandler} = require("./handlers/routeHandlers/userHandler")
-const {tokenHandler} = require("./handlers/routeHandlers/tokenHandler")
-const {checkHandler} = require("./handlers/routeHandlers/checkHandler")
+const { userHandler } = require("./handlers/routeHandlers/userHandler");
+const { tokenHandler } = require("./handlers/routeHandlers/tokenHandler");
+const { checkHandler } = require("./handlers/routeHandlers/checkHandler");
 
 // Module scaffolding
 const routes = {
-    "" : sampleHandler,
-    "sample" : sampleHandler,
-    "user" : userHandler,
-    "token" : tokenHandler,
-    "check" : checkHandler,
+  user: userHandler,
+  token: tokenHandler,
+  check: checkHandler,
 };
 
 module.exports = routes;
